83667d2046f362a51644ac12aa893c9907017ec0
Nashunn <boullet.nicolas@gmail.com>
2018-05-16 17:26:52 +0200

    small change on card

--- travelFront/src/components/CardBook.vue
+++ travelFront/src/components/CardBook.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Card (book version) -->
   <div class="tb-card">
-    <img :style="{ 'backgroundImage': 'url(' + imgSrc + ')' }" class="card-img col m5 s5" alt="Card image">
+      <div :style="{ 'backgroundImage': 'url(' + imgSrc + ')' }" class="card-img col m5 s5"></div>
     <div class="card-desc col m7 s7 row">
       <div class="header m12">
 
