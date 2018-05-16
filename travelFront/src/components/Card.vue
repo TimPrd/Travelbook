580a26dc0869ef701fec2d23013e3b707d378cfb
Nashunn <boullet.nicolas@gmail.com>
2018-05-16 21:46:22 +0200

    changement on card

--- travelFront/src/components/Card.vue
+++ travelFront/src/components/Card.vue
@@ -8,14 +8,7 @@
         <h3 class="title m12">{{card.title}}</h3>
         <img src="#" class="cat-ico m3" alt=""/>
         <div class="localisation bold italic m9">{{card.adress}}</div>
-        <div class="category bold m9">{{card.category.toUpperCase() || "Non reconnue"}}</div>
-        <div class="score">
-          <i class="fas fa-star star"></i><!-- Full star -->
-          <i class="fas fa-star star"></i><!-- Full star -->
-          <i class="fas fa-star star"></i><!-- Full star -->
-          <i class="fas fa-star star"></i><!-- Full star -->
-          <i class="far fa-star star"></i><!-- Empty star -->
-        </div>
+        <div class="category bold m9">{{card.category || "Non reconnue"}}</div>
       </div>
       <div class="summary m12">
         {{card.body}}
